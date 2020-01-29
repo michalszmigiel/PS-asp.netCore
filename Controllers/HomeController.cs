a649140decd78b3f029f44a65cb7d3a5321022ed
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-01-29 19:04:41 +0100

    added routes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,5 +32,15 @@ namespace PS.Controllers
 
             return View(homeViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie == null)
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
     }
 }
